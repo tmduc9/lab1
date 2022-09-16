1a8d25e65264444c4d9bfa719b1c003e56266024
tmduc9 <ducxter@gmail.com>
2022-09-16 12:32:04 -0700

    new file

--- tmp.py
+++ tmp.py
@@ -3,25 +3,19 @@ import picar_4wd as fc
 speed = 30
 
 def main():
-    while True :
-        fc.forward(speed)
-        fc.time.sleep(0.4)
-        fc.turn_left(speed)
-        fc.time.sleep(0.4)
-        fc.turn_left(speed)
-        fc.time.sleep(0.4)
-        fc.turn_left(speed)
-        fc.time.sleep(1)
-        fc.stop()
-        fc.backward(speed)
-        fc.time.sleep(0.4)
-        fc.turn_right(speed)
-        fc.time.sleep(0.4)
-        fc.turn_right(speed)
-        fc.time.sleep(0.4)
-        fc.turn_right(speed)
-        fc.stop()
-        fc.time.sleep(1)
+
+    fc.forward(speed)
+    fc.time.sleep(2)
+    fc.turn_left(speed)
+    fc.time.sleep(2)
+    fc.forward(speed)
+    fc.turn_left(speed)
+    fc.time.sleep(2)
+    fc.forward(speed)
+    fc.time.sleep(2)
+    fc.turn_left(speed)
+    fc.time.sleep(2)
+    fc.forward(speed)
 
 if __name__ == "__main__":
     try:
